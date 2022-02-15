97b99ec8faebc3fa83f62f6cc59e183d545a2c2d
Ahmed Nawaz <ahmednawaz.dev@gmail.com>
2022-02-16 02:37:48 +0500

    brain training excercise done

--- lib/views/explore/brain_training_list.dart
+++ lib/views/explore/brain_training_list.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:psychotherapy_chatbot/constants/colors.dart';
 import 'package:psychotherapy_chatbot/controllers/brain_training_temp.dart';
 import 'package:psychotherapy_chatbot/models/brain_training.dart';
 
 class BrainTrainingList extends StatelessWidget {
   BrainTrainingList({Key? key}) : super(key: key);
-
-  List<MaterialColor> cardColors = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.yellow,
-    Colors.orange,
-    Colors.purple,
-    Colors.pink,
-    Colors.teal,
-    Colors.indigo,
-    Colors.lime,
-    Colors.cyan,
-    Colors.brown,
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -57,61 +43,120 @@ class BrainCard extends StatelessWidget {
 
   BrainTraining? brainExcercise;
 
+  void showSteps(BuildContext context) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        context: context,
+        builder: (context) {
+          return Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 25, bottom: 10),
+                  child: Text(brainExcercise!.title!,
+                      style: Theme.of(context).textTheme.headline1),
+                ),
+                const Divider(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: brainExcercise!.steps!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                            backgroundColor: blue,
+                            child: Text(
+                              "${index + 1}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(color: Colors.white),
+                            )),
+                        title: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            brainExcercise!.steps![index],
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.2,
       margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-      child: Card(
-        elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-          child: Row(children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    elevation: 1,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(brainExcercise!.category),
+      child: InkWell(
+        onTap: () {
+          showSteps(context);
+        },
+        child: Card(
+          elevation: 0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: Row(children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      elevation: 1,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(brainExcercise!.category),
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Text(
-                      brainExcercise!.title!,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .copyWith(fontSize: 22),
+                    Container(
+                      child: Text(
+                        brainExcercise!.title!,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1!
+                            .copyWith(fontSize: 22),
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Text(
-                      brainExcercise!.description!,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1!
-                          .copyWith(fontSize: 13),
-                    ),
-                  )
-                ],
+                    Container(
+                      child: Text(
+                        brainExcercise!.description!,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontSize: 13),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            FittedBox(
-              fit: BoxFit.contain,
-              alignment: Alignment.topRight,
-              child: Image.asset(
-                brainExcercise!.image!,
-                width: 125,
-                height: 125,
-              ),
-            )
-          ]),
+              FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  brainExcercise!.image!,
+                  width: 125,
+                  height: 125,
+                ),
+              )
+            ]),
+          ),
         ),
       ),
     );
